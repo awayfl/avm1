426ed86cb506a00c654b44a555132f591dc5fbc5
Rob Bateman <rob.bateman@gmail.com>
2020-02-25 02:03:45 +0000

    update to swf-loader

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 
 
-console.debug("AwayFL - 0.1.1");
+console.debug("AwayFL - AVM1 - 0.1.1");
 
 export {AVMAwayStage} from "./lib/AVMAwayStage";
 export {AVM1SceneGraphFactory} from "./lib/AVM1SceneGraphFactory";
