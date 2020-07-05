4b9acc86abe8f6620fa6729b53efdfe8ddcdbdb9
Exponenta <rondo.devil@gmail.com>
2020-07-05 16:24:57 +0300

    Unsafe! Return false when get_root return null

--- lib/lib/AVM1MovieClip.ts
+++ lib/lib/AVM1MovieClip.ts
@@ -1129,8 +1129,15 @@ export class AVM1MovieClip extends AVM1SymbolBase<MovieClip> implements IMovieCl
 		}
 		x = alToNumber(this.context, x);
 		y = alToNumber(this.context, y);
-		x += this.get_root().get_x();
-		y += this.get_root().get_y();
+
+		const r = this.get_root();
+		if(!r) {
+			console.warn("[AVM1MovieClip:: hitTest] Root return undef! Return false to prevent crash!")
+			return false;
+		}
+
+		x += r.get_x();
+		y += r.get_y();
 		shapeFlag = alToBoolean(this.context, shapeFlag);
 		return PickGroup.getInstance((<AVM1Stage>this.context.globals.Stage).avmStage.scene.renderer.view).getBoundsPicker(this.adaptee.partition).hitTestPoint(x, y, shapeFlag);
 	}
