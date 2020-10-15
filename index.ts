4c153df0042718a91d7fdbbbbf78f1006beac5ff
Konstantin Timoshenko <rondo.devil@gmail.com>
2020-10-15 15:41:28 +0300

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 
 
-console.debug("AwayFL - AVM1 - 0.2.71");
+console.debug("AwayFL - AVM1 - 0.2.72");
 
 export {AVM1Handler} from "./lib/AVM1Handler";
 export {AVM1SceneGraphFactory} from "./lib/AVM1SceneGraphFactory";
