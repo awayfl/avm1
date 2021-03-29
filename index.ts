a464f5e40d9a46c0a8cc0bc8371e565b72ae8592
Rob Bateman <rob.bateman@gmail.com>
2021-03-29 14:10:01 +0100

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 
 
-console.debug("AwayFL - AVM1 - 0.2.109");
+console.debug("AwayFL - AVM1 - 0.2.110");
 
 export {AVM1Handler} from "./lib/AVM1Handler";
 export {AVM1SceneGraphFactory} from "./lib/AVM1SceneGraphFactory";
