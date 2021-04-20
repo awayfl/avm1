6da74faf4e499c983dbe854d76415fcb909d72c8
Konstantin Timoshenko <rondo.devil@gmail.com>
2021-04-20 16:21:18 +0300

    fix(xml): Use `syntheticRoot` for avoid parser error
    Wrap XML to tag for avoiding parsing error when xml not has valid root.
    For example:
    `<node1></node1><node2></node2>` invalid XML because `root` node MUST be a single tag.
    Wrap it to `<syntheticRoot> <node1></node1><node2></node2> </syntheticRoot>` for avoid any errors.
    
    fix for: https://github.com/awaystudios/nitrome-conversions-batch5/issues/27

--- lib/lib/AVM1XML.ts
+++ lib/lib/AVM1XML.ts
@@ -546,16 +546,24 @@ class AVM1XMLPrototype extends AVM1Object implements IAVM1DataObject {
 		value = alCoerceString(this.context, value);
 
 		const oParser = new DOMParser();
-		const as3Doc = <any>oParser.parseFromString(value, 'application/xml');
+		const as3Doc = oParser.parseFromString('<syntheticRoot>' + value + '</syntheticRoot>', 'application/xml');
 		const error = as3Doc.getElementsByTagName('parsererror');
 
 		if (error.length > 0) {
-			console.warn('[AVM1XML] Parsing error!');
+			console.error('[AVM1XML] Parsing error!');
 			console.groupCollapsed(error[0].textContent);
 			console.log(value);
 			console.groupEnd();
+
+			return;
 		}
-		this.as3XMLDocument = <any>as3Doc;
+
+		const root = as3Doc.childNodes[0];
+
+		AVM1XMLNodePrototype.prototype.initializeFromAS3Node.call(this, root);
+
+		this.as3XMLDocument = <any>root;
+
 		/*this.as3XMLDocument.axSetPublicProperty('ignoreWhite',
 			alToBoolean(this.context, this.alGet('ignoreWhite')));
 		this.as3XMLDocument.axCallPublicProperty('parseXML', [value]);*/
