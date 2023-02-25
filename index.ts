64dd1dc99dc8f1a880c8c54c605d8787359b5fc7
Rob Bateman <rob@infiniteturtles.co.uk>
2023-02-25 00:47:49 +0000

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 
-console.debug('AwayFL - 0.2.152');
+console.debug('AwayFL - 0.2.153');
 
 export { AVM1Handler } from './lib/AVM1Handler';
 export { AVM1SceneGraphFactory } from './lib/AVM1SceneGraphFactory';
