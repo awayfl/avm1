7d89f47a28389918569da31e66fd7bdc42895cef
nosamu <71368227+n0samu@users.noreply.github.com>
2023-04-27 18:29:39 -0500

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 
-console.debug('AwayFL - 0.2.158');
+console.debug('AwayFL - 0.2.159');
 
 export { AVM1Handler } from './lib/AVM1Handler';
 export { AVM1SceneGraphFactory } from './lib/AVM1SceneGraphFactory';
