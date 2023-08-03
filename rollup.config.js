be20980337226b2305d4502527d8e2db66a49f69
Rob Bateman <rob@infiniteturtles.co.uk>
2023-08-03 01:25:30 +0100

    update rollup to latest, switch to terser from uglifyjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,38 @@
-var includePaths = require('rollup-plugin-includepaths');
-var commonjs = require('rollup-plugin-commonjs');
-var nodeResolve = require('rollup-plugin-node-resolve');
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
 
-module.exports = {
+export default {
 	input: './dist/index.js',
 	output: {
 		name: 'AwayflAvm1',
-		sourcemap: true,
-		format: 'umd',
-		file: './bundle/awayfl-avm1.umd.js',
 		globals: {
 			'@awayfl/swf-loader': 'AwayflSwfLoader',
 			'@awayjs/core': 'AwayjsCore',
-			'@awayjs/graphics': 'AwayjsGraphics',
-			'@awayjs/materials': 'AwayjsMaterials',
-			'@awayjs/renderer': 'AwayjsRenderer',
-			'@awayjs/scene': 'AwayjsScene',
 			'@awayjs/stage': 'AwayjsStage',
 			'@awayjs/view': 'AwayjsView',
+			'@awayjs/renderer': 'AwayjsRenderer',
+			'@awayjs/graphics': 'AwayjsGraphics',
+			'@awayjs/materials': 'AwayjsMaterials',
+			'@awayjs/scene': 'AwayjsScene'
 		},
+		sourcemap: true,
+		format: 'umd',
+		file: './bundle/awayfl-avm1.umd.js'
 	},
 	external: [
 		'@awayfl/swf-loader',
 		'@awayjs/core',
-		'@awayjs/graphics',
-		'@awayjs/materials',
-		'@awayjs/renderer',
-		'@awayjs/scene',
 		'@awayjs/stage',
 		'@awayjs/view',
+		'@awayjs/renderer',
+		'@awayjs/graphics',
+		'@awayjs/materials',
+		'@awayjs/scene'
 	],
 	plugins: [
-		nodeResolve({
-			jsnext: true,
-			main: true,
-			module: true
-		}),
-		commonjs({
-			namedExports: {
-				'node_modules/random-seed/index.js': [ 'create' ]
-			},
-			include: /node_modules/
-		}) ]
+		nodeResolve(),
+		commonjs(),
+		terser(),
+	]
 };
